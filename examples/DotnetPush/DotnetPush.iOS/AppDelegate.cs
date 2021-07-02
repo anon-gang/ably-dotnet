b260b2fab23e45af61105e45f50778c638cd4ba4
Martin Georgiev <marto83@gmail.com>
2021-07-02 18:08:58 +0100

    update DotnetPush ios sample app

--- examples/DotnetPush/DotnetPush.iOS/AppDelegate.cs
+++ examples/DotnetPush/DotnetPush.iOS/AppDelegate.cs
@@ -3,18 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using IO.Ably;
+using IO.Ably.Push.iOS;
 using UIKit;
+using Xamarin.Essentials;
 
 namespace DotnetPush.iOS
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to
     // application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        private AblyRealtime _realtime;
+        private AppLoggerSink _loggerSink;
+
+        private void InitialiseAbly()
+        {
+            _loggerSink = new AppLoggerSink();
+
+            AblyAppleMobileDevice.Initialise();
+            var savedClientId = Preferences.Get("ABLY_CLIENT_ID", "", "Ably_Device");
+            var options = new ClientOptions();
+            options.Key = "GJvITg.Jnks6w:IUoxrgaHjIw5LHQG"; // TODO: Remove and delete Martin's app.
+            options.LogHandler = (ILoggerSink)_loggerSink;
+            options.LogLevel = LogLevel.Debug;
+            // This is just to make testing easier.
+            // In a normal app this will usually be set to Secure.GetString(ContentResolver, Secure.AndroidId);
+            if (string.IsNullOrWhiteSpace(savedClientId) == false)
+            {
+                options.ClientId = savedClientId;
+            }
+            else
+            {
+                options.ClientId = Guid.NewGuid().ToString("D");
+            }
+            _realtime = new AblyRealtime(options);
+            _realtime.Connect();
+        }
+
         //
-        // This method is invoked when the application has loaded and is ready to run. In this 
+        // This method is invoked when the application has loaded and is ready to run. In this
         // method you should instantiate the window, load the UI into it and then make the window
         // visible.
         //
@@ -23,9 +53,36 @@ namespace DotnetPush.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
+            LoadApplication(new App(_realtime, _loggerSink));
 
             return base.FinishedLaunching(app, options);
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            var token = deviceToken.ToString();
+            AblyAppleMobileDevice.OnNewRegistrationToken(token, _realtime);
+        }
+
+        public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+        {
+            var ablyError =
+                new ErrorInfo($"Failed to get Registration token for push notifications: {error.LocalizedDescription}");
+
+            AblyAppleMobileDevice.OnRegistrationTokenFailed(ablyError, _realtime);
+        }
+
+        public void ShowErrorMessage(string message)
+        {
+            var alertWindow = new UIWindow(UIScreen.MainScreen.Bounds);
+            alertWindow.RootViewController = new UIViewController();
+
+            var alertController = UIAlertController.Create("Error", message, UIAlertControllerStyle.Alert);
+            alertController.AddAction(UIAlertAction.Create("Close", UIAlertActionStyle.Cancel, _ => alertWindow.Hidden = true));
+
+            alertWindow.WindowLevel = UIWindowLevel.Alert + 1;
+            alertWindow.MakeKeyAndVisible();
+            alertWindow.RootViewController?.PresentViewController(alertController, true, null);
         }
     }
 }
